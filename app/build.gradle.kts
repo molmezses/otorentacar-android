0fbc5ce46105adb1e8a140714ad57899e9de50c2
Eda Dursun <edadrsn929@gmail.com>
2026-05-15 20:54:47 +0300

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.edadursun.otorentacar"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -60,6 +60,12 @@ android {
     buildFeatures {
         viewBinding = true
         buildConfig = true
+    }
+
+    bundle {
+        language {
+            enableSplit = false
+        }
     }
 
 }
